0fab93474820d5a2353b59f8346e9c564b924e40
Arlen Li <329603359@qq.com>
2021-02-13 16:36:09 +1100

    Add MinimumHeightTrees

---
+++ src/main/java/MinimumHeightTrees.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class MinimumHeightTrees {
+//    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+//
+//    }
+
+    class Node{
+        List<Node> Children;
+        boolean visited;
+    }
+
+
+
+}
