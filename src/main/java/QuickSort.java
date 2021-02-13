b86796b614a418295cd838312062f642c5c647ab
Arlen Li <329603359@qq.com>
2021-02-13 16:41:01 +1100

    Refactor QuickSort

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -7,30 +7,18 @@ public class QuickSort {
     }
 
     public static void quickSortAlgo(int low, int high, int[] arr){
-        int pivotIndex = partition(low, high, arr);
-        if(pivotIndex >= 0) {
-            quickSortAlgo(low, pivotIndex - 1, arr);
-            quickSortAlgo(pivotIndex + 1, high, arr);
-        }
-    }
-
-    public static int partition(int low, int high, int[] arr){
-        int pivotIndex = -1;
-        if(low < high){
-            Random random = new Random();
-            int randomIndex = random.nextInt(high + 1 - low) + low;
-            swap(low, randomIndex, arr);
-            int pivot = arr[low];
-            pivotIndex = low + 1;
-            for(int k = low + 1; k <= high; k++){
-                if(arr[k] < pivot){
-                    swap(k, pivotIndex, arr);
-                    pivotIndex++;
-                }
-            }
-            swap(low, pivotIndex - 1, arr);
-        }
-        return pivotIndex - 1;
+      if(low < high){
+           int pivot = arr[low];
+           int pivotIndex = low + 1;
+           for(int i = low + 1; i <= high; i++){
+               if(arr[i] < pivot){
+                   swap(i, pivotIndex++, arr);
+               }
+           }
+           swap(low, --pivotIndex, arr);
+          quickSortAlgo(low, pivotIndex - 1, arr);
+          quickSortAlgo(pivotIndex + 1, high, arr);
+      }
     }
 
     private static void swap(int firstIndex, int secondIndex, int[] arr){
