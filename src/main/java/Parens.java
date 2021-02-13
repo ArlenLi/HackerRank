1bb1858ee90c1c1a6ed42203b966722bcd1ccd73
Arlen Li <329603359@qq.com>
2021-02-13 16:40:37 +1100

    Refactor Parens

--- src/main/java/Parens.java
+++ src/main/java/Parens.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Parens {
     public static List<String> algo(int input){
@@ -26,6 +27,38 @@ public class Parens {
 
             arr[index] = ')';
             addParent(result, remainingLeft, remainingRight - 1, arr, index + 1);
+        }
+    }
+
+    public static List<String> algo1(int input){
+        List<String> result = new ArrayList<>();
+        if(input < 0){
+            throw new IllegalArgumentException();
+        }else if(input == 0){
+            return result;
+        }else{
+            StringBuilder sb = new StringBuilder();
+            addParent(result, sb, input, input);
+            return result;
+        }
+    }
+
+    private static void addParent(List<String> result, StringBuilder sb, int leftRemaining, int rightRemaining){
+        if(leftRemaining == 0 && rightRemaining == 0){
+            result.add(sb.toString());
+            return;
+        }
+
+        if(leftRemaining > 0){
+            sb.append('(');
+            addParent(result, sb, leftRemaining - 1, rightRemaining);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if(rightRemaining > 0 && rightRemaining > leftRemaining){
+            sb.append(')');
+            addParent(result, sb, leftRemaining, rightRemaining - 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
--- src/test/java/ParensTest.java
+++ src/test/java/ParensTest.java
@@ -7,14 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ParensTest {
     @Test
     public void parensAlgo_WhenInputIsZero_ReturnEmptyList(){
-        List<String> actual = Parens.algo(0);
+        List<String> actual = Parens.algo1(0);
 
         assertThat(actual).isEmpty();
     }
 
     @Test
     public void parensAlgo_WhenInputIsThree_ReturnValidParentheses(){
-        List<String> actual = Parens.algo(3);
+        List<String> actual = Parens.algo1(3);
 
         assertThat(actual).containsExactlyInAnyOrder("()()()", "(()())","(())()","((()))","()(())");
     }
